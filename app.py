3dcd0d2c021d99b1d83568ec680718ad8ba69b6e
Ai0318682 <aimanasghar82@gmail.com>
2026-08-06 18:35:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
+import gspread
+from google.oauth2.service_account import credentials
+from datatime import datatime
+#1. Google sheet connect
+scope=["https:\\www.googleapis.com\auth\spreadsheets","https:\\www.googleapis.com\auth\drive"]
+creds=credentials.form_service_account_file("student-form-app-504712-676cf390c482.json",scopes=scope)
+client=gspread.authorize(creds)
+
+Sheet_Id="1XC2VdhAjfwIW8QFZhoNDHE9pY6Zp2aFwXouTck--22Y"
+sheet=client.open_by_key(Sheet_Id).sheet1
+#2. visitors count wala tab
+try:
+    counter_sheet=client.open_by_key(Sheet_Id).worksheet("counter")
+    counter_sheet.update("A1",0)
+
+# har bar page load ho tu +1 karo
+count=int(counter_sheet.acell("A1").value)
+counter_sheet.update("A1",count)
+# sidebar mn count dikhao
+st.sidebar.title("stats")
+st.sidebar.metric("total visitors",count)
+
+st.title("student admission form")
+with st. form("student_form")
 name=st.text_input("enter your name:")
 fname=st.text_input("Enter your father name:")
 adr=st.text_area("Enter your Address:")
@@ -8,8 +32,10 @@ cnic=st.text_input("Enter your CNIC:")
 
 
 
-button=st.button("Done")
-if button:
+submitted=st.form_submit_button("Done")
+if submitted:
+    timestamp=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sheet.append_row([timestamp,name,fname,adr,age,classdate,cnic])
+    
     st.success("form submitted successfully!")
-    st.markdown(f"name:{name},father name:{fname},address:{adr},age:{age},class:{classdata},cnic:{cnic}")
    

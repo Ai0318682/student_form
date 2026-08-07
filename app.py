73b3ce07f7b317c585b1bed5bdab0f403de4001f
Ai0318682 <aimanasghar82@gmail.com>
2026-08-07 19:34:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.set_page_config(page_title="Student Registration Form", page_icon="📝")
 
 # Google Sheets connect
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-
+import json
 creds = Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"], scopes=scope
+ json.loads(st.secrets)["gcp_service_account_json"], scopes=scope
 )
 client = gspread.authorize(creds)
 

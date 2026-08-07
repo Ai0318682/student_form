717d043fcb873995e45a7fefd5635b79b8473544
Ai0318682 <aimanasghar82@gmail.com>
2026-08-07 18:43:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import gspread
 import pandas as pd 
-from google.oauth2.service_account 
+from google.oauth2 import service_account 
 st.set_page_config(page_title="student form",page_icon="", layout="centered")
 #===color+design====
 st.markdown("""
